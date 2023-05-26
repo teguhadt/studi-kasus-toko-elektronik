2a5f7a939a818269e2589b5b2be61851fae10f51
teguhadt <teguhaditia298@gmail.com>
2023-05-26 20:05:23 +0700

    add footer

---
+++ footer.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Footer</title>
+  <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!-- js -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <!-- font awesome -->
+    <script src="https://kit.fontawesome.com/e021723377.js" crossorigin="anonymous"></script>
+</head>
+<body>
+  <footer class="bg-dark text-white mt-5 p-5 ">
+      <div class="row" href="#">
+        <div class="col-md-3">
+          <h5>Layanan Pelanggan</h5>
+          <ul>
+            <li><a class="nav-link active text-light" aria-current="page" href="#">Pusat Pelanggan</a></li>
+            <li><a class="nav-link active text-light" aria-current="page" href="#">Cara Pembelian</a></li>
+            <li><a class="nav-link active text-light" aria-current="page" href="#">Pengembalian</a></li>
+            <li><a class="nav-link active text-light" aria-current="page" href="#">Cara Pengembalian</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h5>Tentang Kami</h5>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore amet odit fuga deserunt reprehenderit ex, architecto pariatur porro voluptates fugiat omnis ullam nobis nulla delectus, veritatis nihil voluptatem atque ratione.
+          </p>
+        </div>
+        <div class="col-md-3">
+          <h5>Mitra Kerjsama</h5>
+          <ul>
+            <li><a class="nav-link active text-light" aria-current="page" href="#">Gojek</a></li>
+            <li><a class="nav-link active text-light" aria-current="page" href="#">Grab</a></li>
+            <li><a class="nav-link active text-light" aria-current="page" href="#">JNE</a></li>
+            <li><a class="nav-link active text-light" aria-current="page" href="#">JNT</a></li>
+            <li><a class="nav-link active text-light" aria-current="page" href="#">Indrive</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h5>Hubungi Kami</h5>
+          <ul>
+            <li><a class="nav-link active text-light" aria-current="page" href="#">+6283820064496</a></li>
+            <li><a class="nav-link active text-light" aria-current="page" href="#">teguhaditia298@gmail.com</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+
+    <div style="background-color: #695CFE;" class="copyright text-center text-white font-weight-bold p-2">
+      <p>Teguh Aditia <i class="far fa-copyright"></i> 2023</p>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  
+</body>
+</html>
